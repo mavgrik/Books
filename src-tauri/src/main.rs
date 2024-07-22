dfa04efa5c9b3924d391da0f5a2290673205071e
mavgrik <79848516+mavgrik@users.noreply.github.com>
2024-07-22 13:14:22 +0200

    Added plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,6 +8,18 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let dsn = std::env::var("https://c71cc8cacbe75cb8da3839e6ef10df70@o4507644748759040.ingest.de.sentry.io/4507644801712208").unwrap_or_else(|_| String::new());
+
+    let client = tauri_plugin_sentry::sentry::init((
+        dsn.clone(),
+        tauri_plugin_sentry::sentry::ClientOptions {
+            debug: true,
+            release: tauri_plugin_sentry::sentry::release_name!(),
+            ..Default::default()
+        },
+    ));
+    let _guard = tauri_plugin_sentry::minidump::init(&client);
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
