66ce95b37d213aa2b80355eb322c46092cb3555f
mavgrik <79848516+mavgrik@users.noreply.github.com>
2024-07-22 15:13:08 +0200

    Finished sentry integration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,16 +4,16 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {}! You've been greeted from Rust!", name);
 }
 
 fn main() {
-    let dsn = std::env::var("https://c71cc8cacbe75cb8da3839e6ef10df70@o4507644748759040.ingest.de.sentry.io/4507644801712208").unwrap_or_else(|_| String::new());
+    dotenv::dotenv().ok();
+    let dsn = std::env::var("SENTRY_DSN").unwrap_or_else(|_| String::new());
 
     let client = tauri_plugin_sentry::sentry::init((
         dsn.clone(),
         tauri_plugin_sentry::sentry::ClientOptions {
-            debug: true,
             release: tauri_plugin_sentry::sentry::release_name!(),
             ..Default::default()
         },
@@ -21,6 +21,7 @@ fn main() {
     let _guard = tauri_plugin_sentry::minidump::init(&client);
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_sentry::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
